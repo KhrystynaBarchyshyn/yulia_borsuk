cb80072c38dd318943ac3d4edd19e32cd1726546
khrystynabarchyshyn <khrystyna.barchyshyn@gmail.com>
2016-04-15 21:19:41 +0000

    smth changed

--- scripts/script_common.js
+++ scripts/script_common.js
@@ -1,1 +1,8 @@
-// JavaScript File
+$(document).ready(function(){
+    $("#nav_icon").click(function(){
+        $(".for_mobile").hide(500);
+    });
+    $("#nav_icon").click(function(){
+        $(".for_mobile").show();
+    });
+});
